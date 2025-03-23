3503cfbab951eca478870fbf204b5b0dbd666b70
WaterMagent <3846359425@qq.com>
2025-03-23 13:45:44 +0800

    name & title

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ export default defineSiteConfig({
   lang: 'zh-CN',
   title: 'Valaxy Theme Yun',
   author: {
-    name: '云游君',
+    name: '水磁WaterMagent',
   },
   description: 'Valaxy Theme Yun Preview.',
   social: [
