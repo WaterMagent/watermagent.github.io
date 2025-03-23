35fe7ffac12c8b3272fa3bee32fe2a32adf74c3c
WaterMagent <3846359425@qq.com>
2025-03-23 19:41:11 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,6 +6,7 @@ export default defineSiteConfig({
   title: 'Valaxy Theme Yun',
   author: {
     name: '水磁WaterMagent',
+    avatar: 'https://cdn.luogu.com.cn/upload/image_hosting/nk6svehs.png'
   },
   description: 'Valaxy Theme Yun Preview.',
   social: [
@@ -16,76 +17,34 @@ export default defineSiteConfig({
       color: 'orange',
     },
     {
-      name: 'QQ 群 1050458482',
-      link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
+      name: 'QQ 群 1033897254',
+      link: 'https://qm.qq.com/cgi-bin/qm/qr?k=Rt5ksLB0ONsfQ1B7YEaEir0LjHJpbVus&jump_from=webapi&authKey=Vpuj6kufgv7V9naXPgY0HjgAA8Nw32eEk9z5RcGm/hJkSLW6DXDrfyTM6y9b8xtz',
       icon: 'i-ri-qq-line',
       color: '#12B7F5',
     },
     {
       name: 'GitHub',
-      link: 'https://github.com/YunYouJun',
+      link: 'https://github.com/WaterMagent',
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
     {
-      name: '微博',
-      link: 'https://weibo.com/jizhideyunyoujun',
-      icon: 'i-ri-weibo-line',
+      name: '洛谷',
+      link: 'https://www.luogu.com.cn/user/1429721',
+      icon: 'i-ri-luogu-line',
       color: '#E6162D',
     },
     {
-      name: '豆瓣',
-      link: 'https://www.douban.com/people/yunyoujun/',
-      icon: 'i-ri-douban-line',
-      color: '#007722',
-    },
-    {
-      name: '网易云音乐',
-      link: 'https://music.163.com/#/user/home?id=247102977',
-      icon: 'i-ri-netease-cloud-music-line',
-      color: '#C20C0C',
-    },
-    {
-      name: '知乎',
-      link: 'https://www.zhihu.com/people/yunyoujun/',
-      icon: 'i-ri-zhihu-line',
-      color: '#0084FF',
-    },
-    {
       name: '哔哩哔哩',
-      link: 'https://space.bilibili.com/1579790',
+      link: 'https://space.bilibili.com/3546745348819092',
       icon: 'i-ri-bilibili-line',
       color: '#FF8EB3',
-    },
-    {
-      name: '微信公众号',
-      link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
-      icon: 'i-ri-wechat-2-line',
-      color: '#1AAD19',
-    },
-    {
-      name: 'Twitter',
-      link: 'https://twitter.com/YunYouJun',
-      icon: 'i-ri-twitter-x-fill',
-      color: 'black',
-    },
-    {
-      name: 'Telegram Channel',
-      link: 'https://t.me/elpsycn',
-      icon: 'i-ri-telegram-line',
-      color: '#0088CC',
     },
     {
       name: 'E-Mail',
       link: 'mailto:me@yunyoujun.cn',
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
-    },
-    {
-      name: 'Travelling',
-      link: 'https://www.travellings.cn/go.html',
-      icon: 'i-ri-train-line',
-      color: 'var(--va-c-text)',
     },
   ],
 
